df719b243e3174660cf2e5c0c114eec64e65da6b
reahaas <rea@scadafence.com>
2021-07-21 03:04:23 +0300

    Add attack logic and tests

--- db_service/db_python.py
+++ db_service/db_python.py
@@ -1,3 +1,10 @@
+import itertools
+
+import networkx as nx
+
+from common_utils import flatten, remove_duplication
+
+
 def setup_db():
     return DbPython()
 
@@ -5,7 +12,58 @@ def setup_db():
 class DbPython():
     def __init__(self):
         self.vms_to_potentially_attackers = {}
+        self.tag_to_potentially_attackers = {}
         self.requests_stats = {}
         
+        # For more information visit: https://networkx.org/documentation/stable/reference/classes/digraph.html
+        self.tags_directed_graph = nx.DiGraph()
+    
     def add_vm(self, vm_id, potential_attackers):
         self.vms_to_potentially_attackers[vm_id] = potential_attackers
+    
+    def get_potentially_attackers(self, vm_id: str) -> list:
+        if vm_id not in self.vms_to_potentially_attackers:
+            raise KeyError(f"{vm_id} not exists in the db.")
+        potentially_attackers = self.vms_to_potentially_attackers.get(vm_id)
+        potentially_attackers_ids = [vm.get("vm_id") for vm in potentially_attackers]
+        return potentially_attackers_ids
+
+
+def save_tag_to_vms(db, tags_to_vms):
+    for tag in tags_to_vms:
+        db[tag] = tags_to_vms[tag]
+
+
+def build_tags_directed_graph(db, fw_rules):
+    for rule in fw_rules:
+        db.tags_directed_graph.add_edge(rule.get("source_tag"), rule.get("dest_tag"))
+
+
+def build_vms_to_potentially_attackers(db, vms):
+    tag_to_allowed_to_access_tags = {}
+    
+    tag_to_potentially_attackers = initialize_tags_tp_potentially_attackers(vms)
+    
+    for tag in db.tags_directed_graph.nodes():
+        tag_to_allowed_to_access_tags[tag] = list(db.tags_directed_graph.predecessors(tag))
+    
+    for tag in db.tags_directed_graph.nodes():
+        tag_to_potentially_attackers[tag] = [vm for vm in vms if any(
+            vm_tag in tag_to_allowed_to_access_tags[tag] for vm_tag in vm.get("tags"))]
+    
+    vms_to_potentially_attackers = {}
+    for vm in vms:
+        vms_to_potentially_attackers[vm.get("vm_id")] = flatten(
+            [tag_to_potentially_attackers[tag] for tag in vm.get("tags")])
+    
+    db.tag_to_potentially_attackers = tag_to_potentially_attackers
+    db.vms_to_potentially_attackers = vms_to_potentially_attackers
+
+
+def initialize_tags_tp_potentially_attackers(vms):
+    tag_to_potentially_attackers = {}
+    all_vms_tags = flatten([vm.get("tags") for vm in vms if vm.get("tags")])
+    set_all_vms_tags = remove_duplication(all_vms_tags)
+    for tag in set_all_vms_tags:
+        tag_to_potentially_attackers[tag] = []
+    return tag_to_potentially_attackers
--- tests/test_runner.py
+++ tests/test_runner.py
@@ -3,10 +3,20 @@ from loader_service.load_attack_surface_data import load_attack_surface_data
 
 
 def test_load_attack_surface_data():
+    """
+    In "input-0.json" scenario there are two vms and one rule.
+    vm_id_2 = 'vm-c7bac01a07' can attack both the vms.
+    :return:
+    """
     input_file = "data/input-0.json"
     
     db = setup_db()
     load_attack_surface_data(input_file, db)
+    vm_id_1 = 'vm-a211de'
+    vm_id_2 = 'vm-c7bac01a07'
     
-    expected_vms_to_potentially_attackers = {'vm-a211de': [], 'vm-c7bac01a07': []}
-    assert db.vms_to_potentially_attackers == expected_vms_to_potentially_attackers
+    expected_vms_to_potentially_attackers_vm_id_1 = [vm_id_2]
+    assert db.get_potentially_attackers(vm_id_1) == expected_vms_to_potentially_attackers_vm_id_1
+    
+    expected_vms_to_potentially_attackers_vm_id_2 = [vm_id_2]
+    assert db.get_potentially_attackers(vm_id_2) == expected_vms_to_potentially_attackers_vm_id_2
